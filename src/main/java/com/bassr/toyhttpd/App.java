029432d37ffd40bb46d8db06142907f70566e624
Chris Johnson <tenorviol@yahoo.com>
2012-08-25 11:13:45 -0700

    initial commit

---
+++ src/main/java/com/bassr/toyhttpd/App.java
@@ -0,0 +1,13 @@
+package com.bassr.toyhttpd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
